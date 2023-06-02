9d0db7b72b45e5dd29e87170388dcdf015df279c
Alperen <134652196+alprsb@users.noreply.github.com>
2023-06-02 04:55:08 -0400

    Update Mac_Changed.py

--- Mac_Changed.py
+++ Mac_Changed.py
@@ -4,14 +4,14 @@ import re
 
 def get_user_input():
     parse_object = optparse.OptionParser()
-    parse_object.add_option("-i", "--interface", dest="interface", help="int_help")
-    parse_object.add_option("-m", "--mac", dest="mac_adress", help="mac_help")
+    parse_object.add_option("-i","--interface",dest ="interface",help ="int_help")
+    parse_object.add_option("-m","--mac_adress",dest = "mac_adress",help ="mac_help")
     return parse_object.parse_args()
 
-def macChanged(user_interface,user_mac_adress ):
-    subprocess.call(["ifconfig", user_interface, "down"])
-    subprocess.call(["ifconfig", user_interface, "hw", "ether", user_mac_adress])
-    subprocess.call(["ifconfig", user_interface, "up"])
+def macChanged(user_interface,user_mac_adress):
+    subprocess.call(["ifconfig",user_interface,"down"])
+    subprocess.call(["ifconfig",user_interface,"hw","ether",user_mac_adress])
+    subprocess.call(["ifconfig",user_interface,"up"])
 def control_new_mac(interface):
     ifconfig = subprocess.check_output(["ifconfig",interface])
     new_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",ifconfig.decode("utf-8"))
@@ -20,13 +20,12 @@ def control_new_mac(interface):
     else:
         return None
 
-print("Mac changed is sucessful")
-(user_input,arguments) =get_user_input()
+print("Mac changed!")
+user_input = get_user_input()[0]
 macChanged(user_input.interface,user_input.mac_adress)
 final_mac = control_new_mac(user_input.interface)
-print(final_mac)
 
 if final_mac == user_input.mac_adress:
-    print("You made it!!")
+    print("Cong. you made it!")
 else:
-    print("I'm sorry you fail :(")
+    print("I am sorry you failed")
